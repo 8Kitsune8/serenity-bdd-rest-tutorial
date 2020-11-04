30294ad63828c69d792f8bf27ae0d4120a21eafc
Kit <nomail@gmail.com>
2020-11-04 17:30:59 +0100

    using RestAssured.baseURI tu use same base uri over all tests and Ensure.that to add information in reports about expected results

---
+++ src/test/java/iex/WhenFetchingUsersInfo.java
@@ -0,0 +1,29 @@
+package iex;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.Ensure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static net.serenitybdd.rest.SerenityRest.when;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SerenityRunner.class)
+public class WhenFetchingUsersInfo {
+
+    @Before
+    public void setBaseUri(){
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void shouldReturnUserNameAndMail(){
+        when().get("/users/1")
+                .then().statusCode(200);
+
+            Ensure.that("user name is returned", response -> response.body("username", equalTo("Bret")));
+            Ensure.that("email is returned", response -> response.body("email", equalTo("Sincere@april.biz")));
+    }
+}
